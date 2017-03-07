83c82bec40b054b7521ddf7a511a39ccb2b03cb5
santhosh <tpsanthu@outlook.com>
2017-03-07 11:34:41 -0500

    git updated

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,12 +6,17 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package "httpd" do
-action :install 
-end 
+package 'Install Apache' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    package_name 'httpd'
+  when 'ubuntu', 'debian'
+    package_name 'apache2'
+  end
+end
 
-service "httpd " do
-action { :enable, :start }
+service "apache2" do
+action [ :enable, :start ]
 end 
 
 cookbook_file "/var/www/html/index.html"do
